1f56ea657753cfa17be6298462802c4a9a50c582
Arthur Corbellini <arthur.corbellini27@gmail.com>
2021-11-05 19:39:40 -0300

    adicao log4j

---
+++ src/main/java/br/univates/kartodromo/TesteLog4j.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.univates.kartodromo;
+
+import org.jboss.logging.Logger;
+
+/**
+ *
+ * @author Arthur
+ */
+public class TesteLog4j {
+
+    final static Logger logger = Logger.getLogger(TesteLog4j.class);
+
+    public static void main(String[] args) {
+        TesteLog4j teste = new TesteLog4j();
+
+        try {
+            teste.dividir();
+        } catch (Exception e) {
+            logger.error("erro ao dividir", e);
+        }
+    }
+
+    private void dividir() {
+        int i = 20 / 0;
+    }
+}
